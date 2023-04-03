60fa20fbddbeeb76cc7fc120ae2dc06e4fcf15ec
meroneyeram <meroneyeram@gmail.com>
2023-04-03 14:19:34 -0700

    Completed App

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Css from "./pages/Css";
+import Javascript from "./pages/Javascript";
+import ReactJS from "./pages/ReactJS";
+import Question from "./components/Question/Question";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+
+function App() {
+  // hello
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/css" />} />
+        <Route path="/css" element={<Css />} />
+        <Route path="/javascript" element={<Javascript />} />
+        <Route path="/react" element={<ReactJS />} />
+        <Route path="/#:id" element={<Question />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
